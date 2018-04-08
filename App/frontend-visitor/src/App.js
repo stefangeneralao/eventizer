ab373dc94da2eb33136daa1f4ae529ae0010286c
stefangeneralao <stefan.generalao@gmail.com>
2018-04-08 19:53:44 +0200

    Replaced App by new design

--- App/frontend-visitor/src/App.js
+++ App/frontend-visitor/src/App.js
@@ -1,58 +1,57 @@
-import React, {Component} from 'react'
-import logo from './images/mau-logo.png'
-import './App.css'
+import React, {Component} from 'react';
+import './App.css';
+import DateCard from './DateCard';
 
-class ProjectCard extends Component {
-  render() {
-		let title = this.props.title || "Ingen titel"
-		let description = this.props.description || "Ingen beskrivning"
+// TEMPORARY!
+// Database-driver. This function will be replaced by database.
+const getDateCards = () => {
+	// The key-attribute will be replaced by the primary key from the database.
+	const dateCards = [
+		<DateCard
+			key="2018-04-03"
+			date="2018-04-03"
+			events={[
+				{position: "NI:3", label: "IA-dagen"},
+				{position: "NI:4", label: "Kåren rådgivning"},
+				{position: "G8:4", label: "Mäklarmässa"}
+			]}
+		/>,
 
-    return (
-			<div className="ProjectCard">
-	      <h3>{title}</h3>
-	      <p>{description}</p>
-				<img
-					src="https://www.sydsvenskan.se/images/NfjgUZvTfUWT51yWQTk2gR8eUdA.jpg"
-					alt={title}
-				/>
-    	</div>
-		)
-  }
+		<DateCard
+			key="2018-04-15"
+			date="2018-04-15"
+			events={[
+				{position: "Kårhuset", label: "Gratis kaffe"}
+			]}
+		/>,
+
+		<DateCard
+			key="2018-06-03"
+			date="2018-06-03"
+			events={[
+				{position: "Orkanen", label: "Dyrt kaffe"}
+			]}
+		/>,
+	];
+
+	return dateCards;
 }
 
+// Main component.
 class App extends Component {
   render() {
     return (
 			<div className="App">
-	      <header className="App-header">
-	        <img src={logo} className="App-logo" alt="logo"/>
-	        <h1 className="App-title">Eventizer</h1>
-	      </header>
-	      <main>
-	        <div className="Card-wrapper">
-		          <ProjectCard
-								title="Exempelprojekt 1"
-								description="Kort beskrivning om projekt 1"
-							/>
-		          <ProjectCard
-								title="Exempelprojekt 2"
-								description="Kort beskrivning om projekt 2"
-							/>
-		          <ProjectCard title="Exempelprojekt 3"/>
-		          <ProjectCard title="Exempelprojekt 4"/>
-		          <ProjectCard title="Exempelprojekt 5"/>
-							<ProjectCard title="Exempelprojekt 6"/>
-							<ProjectCard title="Exempelprojekt 7"/>
-							<ProjectCard title="Exempelprojekt 8"/>
-							<ProjectCard />
-		          <ProjectCard />
-		          <ProjectCard />
-		          <ProjectCard />
-	        </div>
-	      </main>
-    	</div>
-		)
+				<header className="App-header">
+					<h1 className="App-title">Eventizer</h1>
+				</header>
+
+				<main>
+					{getDateCards()}
+				</main>
+			</div>
+		);
   }
 }
 
-export default App
+export default App;
