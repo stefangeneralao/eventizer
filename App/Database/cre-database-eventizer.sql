e1d84e27529a0580e7f84e4a4803ab606d273d1b
Elin Forsnor <elin.forsnor@gmail.com>
2018-04-24 16:15:54 +0200

    updated databasesript

--- App/Database/cre-database-eventizer.sql
+++ App/Database/cre-database-eventizer.sql
@@ -1,3 +1,4 @@
+\c hej;
 DROP DATABASE if exists eventizer;
 CREATE database eventizer;
 \c eventizer;
@@ -9,50 +10,38 @@ create table event(
   eventdate   date,
   starttime   time,
   endtime     time,
-  locationID  serial,
-
-    foreign key (locationID) references location(locationID)
+  locationID  varchar (9)
 );
 
 create table exhibitor(
   exhibitorID   serial primary key,
   eventID       serial,
   title         text,
-  description   text,
-
-  foreign key (eventID) references event(eventID)
+  description   text
 );
 
 create table exhibitorimg(
   imgID         serial primary key,
   img           bytea,
-  exhibitorID   serial,
-
-  foreign key (exhibitorID) references exhibitor(exhibitorID)
+  exhibitorID   serial
 );
 
 create table eventimg(
   imgID       serial primary key,
   img         bytea,
-  eventID     serial,
-
-  foreign key (eventID) references event(eventID)
-);
-
-create table location(
-  locationID  varchar(9) primary key,
-  level       int,
-  buildingID  serial,
-
-  foreign key (buildingID) references building(buildingID)
+  eventID     serial
 );
 
 create table building(
   lat         float,
   long        float,
-  buildingID  serial,
+  buildingID  serial
+);
 
-  primary key (lat, long)
+create table location(
+  locationID  varchar(9) primary key,
+  level       int,
+  buildingID  serial
 );
 
 create table contact(
@@ -64,7 +53,14 @@ create table exhibitorcontact(
   exhibitorID serial,
   email       text,
 
-  primary key (exhibitorID, email),
-  foreign key (exhibitorID) references exhibitor(exhibitorID),
-  foreign key (email) references person(email)
+  primary key (exhibitorID, email)
 );
+
+alter table building add primary key (buildingID);
+alter table event add foreign key (locationID) references location(locationID);
+alter table exhibitor add foreign key (eventID) references event(eventID);
+alter table exhibitorimg add foreign key (exhibitorID) references exhibitor(exhibitorID);
+alter table eventimg add foreign key (eventID) references event(eventID);
+alter table location add constraint fk_building foreign key (buildingID) references building(buildingID);
+alter table exhibitorcontact add foreign key (exhibitorID) references exhibitor(exhibitorID);
+alter table exhibitorcontact add foreign key (email) references contact(email);
