b021b241ad6aa53187cd7530a305eaae8da985d1
Ellen Lindberg <ellenkelindberg@gmail.com>
2018-04-25 20:38:05 +0200

    Changed name from ExhibitorComponent to ExhibitorContainer

--- App/frontend-visitor/src/app/main/routes/event/ExhibitorComponent.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class ExhibitorComponent extends React.Component {
-	render() {
-		return null;
-	}
-}
-
-export default ExhibitorComponent;
---
+++ App/frontend-visitor/src/app/main/routes/event/ExhibitorContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class ExhibitorContainer extends React.Component {
+	render() {
+		return null;
+	}
+}
+
+export default ExhibitorContainer;
