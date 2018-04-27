79a2a9a7e0ce50bc7e2f99c61f2f772bf9ad0384
Elin Forsnor <elin.forsnor@gmail.com>
2018-04-27 19:56:10 +0200

    Added key for imgID

--- App/frontend-visitor/src/app/main/routes/root/EventContainer.js
+++ App/frontend-visitor/src/app/main/routes/root/EventContainer.js
@@ -5,7 +5,7 @@ class EventContainer extends React.Component {
   getEvents() {
     const events = this.props.events.map((event, i) => {
       return (
-        <EventComponent title={event.label} description={event.description} time={event.time} location={event.location} key={i}/>
+        <EventComponent title={event.label} description={event.description} time={event.time} location={event.location} imgID={event.imgID} key={i}/>
       )
     });
     return events;
