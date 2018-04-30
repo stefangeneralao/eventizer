c49a9a762b24abeef3d5e14f1aef2855e83a6385
Ellen Lindberg <ellenkelindberg@gmail.com>
2018-04-30 17:02:55 +0200

    Created link between ExhibitorMiniature and ExhibitorCard

--- App/frontend-visitor/src/app/main/routes/event/ExhibitorMiniature.js
+++ App/frontend-visitor/src/app/main/routes/event/ExhibitorMiniature.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 //Component that display exhibitor miniatures.
 class ExhibitorMiniature extends React.Component {
 
 	render() {
+		const onClickLink = "/exhibitor/" + this.props.label;
+
 		return (
 			<div>
-				<p>{this.props.label}</p>
+				<p><Link to={onClickLink}>{this.props.label}</Link></p>
 				<p>{this.props.description}</p>
 			</div>
 		);
