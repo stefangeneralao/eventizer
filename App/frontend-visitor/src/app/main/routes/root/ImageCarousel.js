8051344f06fdb21fc964e0cb66b38ffb46f62ba4
Elin Forsnor <elin.forsnor@gmail.com>
2018-05-01 10:34:12 +0200

    Added CSS for Slick and imported CSS-file to ImageCarousel

--- App/frontend-visitor/src/app/main/routes/root/ImageCarousel.js
+++ App/frontend-visitor/src/app/main/routes/root/ImageCarousel.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Slider from "react-slick";
+import "../../../../index.css";
 import ia from '../../../../images/ia.jpg';
 import kåren from '../../../../images/kåren.jpg';
 import sopplunch from '../../../../images/sopplunch.jpg';
@@ -6,12 +8,26 @@ import sopplunch from '../../../../images/sopplunch.jpg';
 //Component that returns images for ImageCarousel
 class ImageCarousel extends React.Component {
   render() {
+    var settings = {
+      dots: true
+    };
     return (
       <div className="ImageCarousel">
-      <img src={ia} className="EventImage" alt="ia"/>
-      <img src={kåren} className="EventImage" alt="kåren"/>
-      <img src={sopplunch} className="EventImage" alt="sopplunch"/>
-      </div>
+  <Slider {...settings}>
+    <div>
+    <img src={ia} className="EventImage" alt="ia"/>
+    </div>
+    <div>
+    <img src={kåren} className="EventImage" alt="kåren"/>
+    </div>
+    <div>
+    <img src={sopplunch} className="EventImage" alt="sopplunch"/>
+    </div>
+    <div>
+      <img src="http://placekitten.com/g/400/200" />
+    </div>
+  </Slider>
+</div>
     )
   }
 }
