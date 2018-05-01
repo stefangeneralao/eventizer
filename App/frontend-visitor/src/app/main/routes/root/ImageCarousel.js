2bc106d18bb37d7ba3e65bc20cb00bc88b62779f
stefangeneralao <stefan.generalao@gmail.com>
2018-05-01 16:31:41 +0200

    ImageCarousel importing images correctly

--- App/frontend-visitor/src/app/main/routes/root/ImageCarousel.js
+++ App/frontend-visitor/src/app/main/routes/root/ImageCarousel.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Slider from "react-slick";
 import "../../../../index.css";
-//import ia from '../../../../images/ia.jpg';
+// import ia from '../../../../images/ia.jpg';
 //import kåren from '../../../../images/kåren.jpg';
 //import sopplunch from '../../../../images/sopplunch.jpg';
 
@@ -9,10 +9,10 @@ import "../../../../index.css";
 class ImageCarousel extends React.Component {
   getImageByID () {
     return this.props.imageID.map((imgID, i) => {
-      console.log(this.props.imageID)
+			console.log(imgID);
       return (
         <div key={i}>
-        <img src={"../../../../images/" + imgID} alt=" "/>
+        	<img src={require("../../../../images/" + imgID)} alt=" "/>
         </div>
       );
     })
