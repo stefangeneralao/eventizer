7a4073d5c79e4d7fa4afe4abc8981bbcff5ef744
stefangeneralao <stefan.generalao@gmail.com>
2018-05-03 19:43:46 +0200

    Fixed moreBtn, also implemented CSS-grid

--- App/frontend-visitor/src/app/main/routes/root/EventComponent.js
+++ App/frontend-visitor/src/app/main/routes/root/EventComponent.js
@@ -1,63 +1,97 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import arrow from './icons/arrow.svg';
 
 //Component that renders EventCards with links to events
 class EventComponent extends React.Component {
-  getTitle() {
-    return<div className="EventComponent__event-label">
-      {this.props.title}
-    </div>;
-  }
+	constructor() {
+		super();
 
-  getLocation() {
-    return<div className="EventComponent__event-location">
-      {this.props.location}
-    </div>;
-  }
+		this.state = {descriptionOpen: false};
+	}
 
-  getTime() {
-    return<div className="EventComponent__event-time">
-      {this.props.time}
-    </div>;
-  }
+	getTitle() {
+		return<div className="EventComponent__event-label">
+			{this.props.title}
+		</div>;
+	}
 
-  getDescription() {
-    return<div className="EventComponent__event-description">
-      {this.props.description}
-    </div>;
-  }
+	getLocation() {
+		return<div className="EventComponent__event-location">
+			{this.props.location}
+		</div>;
+	}
 
-  getLink() {
-    if (this.props.exhibitors && this.props.exhibitors.length === 1){
-      return "/exhibitor/" + this.props.exhibitors[0].exhibitionID;
-    }
-    else {
-      return "/event/" + this.props.title;
-    }
-  }
+	getTime() {
+		let className = 'EventComponent__event-time'
+		className += this.state.descriptionOpen ? '' : ' hidden';
 
-  render () {
+		return<div className={className}>
+			{this.props.time}
+		</div>;
+	}
+
+	getDescription() {
+		let className = 'EventComponent__event-description';
+		className += this.state.descriptionOpen ? '' : ' hidden';
+
+		return<div className={className}>
+			{this.props.description}
+		</div>;
+	}
+
+	getLink() {
+		if (this.props.exhibitors && this.props.exhibitors.length === 1){
+			return "/exhibitor/" + this.props.exhibitors[0].exhibitionID;
+		}
+		else {
+			return "/event/" + this.props.title;
+		}
+	}
+
+	onMoreBtnClick() {
+		this.setState({descriptionOpen: !this.state.descriptionOpen});
+	}
+
+	getMoreBtn() {
+		let className = 'EventComponent__moreBtn';
+		className += this.state.descriptionOpen ? ' open' : ' closed';
+
+		return (
+				<img src={arrow} alt="show more" className={className} onClick={() => {this.onMoreBtnClick()}}/>
+		);
+	}
+
+	render () {
 		const title = this.getTitle();
-    const location = this.getLocation();
-    const time = this.getTime();
-    const description = this.getDescription();
-    const onClickLink = this.getLink();
-		return<div className="EventComponent__event-wrapper">
-    <div>
-      {location}
-      <Link to={onClickLink}>
-      {title}
-      </Link>
-    </div>
+		const location = this.getLocation();
+		const time = this.getTime();
+		const description = this.getDescription();
+		const onClickLink = this.getLink();
+		const moreBtn = this.getMoreBtn();
 
-    <div>
-      {time}
-    </div>
+		return (
+			<div className="EventComponent">
+				<Link to={onClickLink}>
+				{title}
+				</Link>
 
-    <div>
-      {description}
-    </div>
-  </div>
+				{location}
+
+				<div>
+					{moreBtn}
+				</div>
+
+				<div>
+					{description}
+				</div>
+
+				<div>
+					{time}
+				</div>
+
+			</div>
+)
 	};
 }
 export default EventComponent;
