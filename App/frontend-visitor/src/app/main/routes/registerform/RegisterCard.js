bb9d5fd25cca43df2375844dfef699a2b293f3da
Ellen Lindberg <ellenkelindberg@gmail.com>
2018-05-16 18:48:55 +0200

    CSS

--- App/frontend-visitor/src/app/main/routes/registerform/RegisterCard.js
+++ App/frontend-visitor/src/app/main/routes/registerform/RegisterCard.js
@@ -96,7 +96,7 @@ class RegisterCard extends React.Component {
 
         axios.post("http://localhost:3001/exhibitor_form_request", fd , config).then(function() {
             console.log("It worked!");
-            alert("Tack! Vi har nu mottagit din information.");
+            alert("Tack! Ditt projekt är nu anmält.");
             /*When the information is submitted, the formElement resets, and the states gets the value "" and null (setState). formElement and componentThis is because this.blablalba refers back to axios.*/
             formElement.reset();
             componentThis.setState({
@@ -124,11 +124,11 @@ class RegisterCard extends React.Component {
 				<div className= "FormWrapper">
 					<h2>Registrera en utställning</h2>
 
-					<fieldgroup className="RegisterTitel">
+					<div className="RegisterTitel">
 						<input
 							type="text"
 							name="titleField"
-							placeholder="Fyll i titel..."
+							placeholder="Projektnamn"
 							required="required"
 							onChange={
 								event => {
@@ -136,45 +136,49 @@ class RegisterCard extends React.Component {
 								}
 							}
 						/>
-					</fieldgroup>
+					</div>
 					<div className="RegisterDescription">
-						<fieldgroup>
-							<textarea cols="50" rows="8"
-								name="descField"
-								placeholder="Beskriv din utställning" required="required"
-								onChange={
-									event => {
-										this.setState({"descField": event.target.value})
-									}
-								}
-							></textarea>
-						</fieldgroup>
+                        <textarea cols="50" rows="8"
+                            name="descField"
+                            placeholder="Beskriv kortfattat din utställning" 
+                            required="required"
+				            onChange={
+                                event => {
+                                this.setState({"descField": event.target.value})
+                                }
+                            }>
+                        </textarea>
 					</div>
 
-					<fieldgroup className="RegisterDate">
-						<label className="RegisterFormLabel">Välj datum nedan:</label>
+					<div className="RegisterDate">
+						<label className="RegisterFormLabel">Välj datum för event nedan:</label>
 						<div>
 							{this.getDateDropDown()}
 						</div>
-					</fieldgroup>
+					</div>
 
-					<fieldgroup className="RegisterEvent">
+					<div className="RegisterEvent">
 						<label className="RegisterFormLabel">Välj event:</label>
 						<div>
 							{this.getEventDropDown()}
 						</div>
-					</fieldgroup>
+					</div>
 
 					<div className="RegisterUploadFiles">
 						<section>
-							<label className="RegisterFormLabel">Lägg till en omslagsbild:</label>
+							<label className="RegisterFormLabel">Lägg till en bild:</label>
 							<div>
-              	<input type="file" name="fileUpload" required="required" onChange={this.fileSelectedHandler}/>
-							</div>
+                                <input 
+                                type="file" 
+                                name="fileUpload" 
+                                required="required"
+                                accept=".jpg, .jpeg, .png"
+                                onChange={this.fileSelectedHandler}/>
+				            </div>
 						</section>
-          </div>
+                    </div>
 
-          <div className="RegisterSubmit">
+                    <div className="RegisterSubmit">
 						<button>Skicka!</button>
 					</div>
 				</div>
