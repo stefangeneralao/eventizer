41298556eb1a91a0d89c299698921378c382575e
Ellen Lindberg <ellenkelindberg@gmail.com>
2018-05-21 14:36:10 +0200

    Code Ref

--- App/frontend-visitor/src/app/main/routes/event/ExhibitorMiniature.js
+++ App/frontend-visitor/src/app/main/routes/event/ExhibitorMiniature.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-//Component that display exhibitor miniatures
+// Component that display exhibitor miniatures
 class ExhibitorMiniature extends React.Component {
-
 	render() {
-		const onClickLink = "/exhibitor/" + this.props.id;
-		const imgURL = "/static/media/" + this.props.image;
+		const onClickLink = '/exhibitor/' + this.props.id;
+		const imgURL = '/static/media/' + this.props.image;
 
 		return (
 			<div className="ExhibitorMiniature">
-				<p><Link to={onClickLink}><img className="ExhibitorMiniatureImage" src={imgURL} alt={this.props.label}/></Link></p>
-				<p><Link to={onClickLink}>{this.props.label}</Link></p>
+				<p>
+					<Link to={onClickLink}>
+						<img
+							className="ExhibitorMiniatureImage"
+							src={imgURL}
+							alt={this.props.label}
+						/>
+					</Link>
+				</p>
+				<p>
+					<Link to={onClickLink}>{this.props.label}</Link>
+				</p>
 			</div>
 		);
 	}
