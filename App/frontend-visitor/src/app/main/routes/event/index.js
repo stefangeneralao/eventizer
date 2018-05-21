64852d0fbf067c01be118c51019924184dfff2eb
Ellen Lindberg <ellenkelindberg@gmail.com>
2018-05-21 15:33:39 +0200

    Code Ref

--- App/frontend-visitor/src/app/main/routes/event/index.js
+++ App/frontend-visitor/src/app/main/routes/event/index.js
@@ -3,24 +3,23 @@ import EventCard from './EventCard';
 
 // This component renders the EventCards
 class Event extends React.Component {
-
 	// Fetches EventId from browser URL
 	getEventIdFromURL() {
 		let eventId = document.URL;
-		eventId = eventId.split('/')
+		eventId = eventId.split('/');
 		eventId = eventId[eventId.length - 1];
 		return eventId;
 	}
 
 	// Function that groups exhibitors by category
-	// Uses the reduce method to loop the exhibitors and grouping them by category. Creating lists of category on a object.
+	// Uses the reduce method to loop the exhibitors and grouping them by category
 	getGroupedExhibitors(exhibitors) {
     return exhibitors.reduce((obj, exhibitor) => {
-				var key = exhibitor.category || "no-category";
-				obj[key] = obj[key] || [];
-        obj[key].push(exhibitor);
-        return obj;
-    }, Object.create(null));
+			var key = exhibitor.category || 'no-category';
+			obj[key] = obj[key] || [];
+      obj[key].push(exhibitor);
+      return obj;
+		}, Object.create(null));
 	}
 
 	// Matches the URL with the right event and returns event data
@@ -28,14 +27,14 @@ class Event extends React.Component {
 		const eventId = this.getEventIdFromURL();
 		const dates = this.props.store.dates;
 
-		//Loops every date to get the events
+		// Loops every date to get the events
 		for(let i in dates) {
 			const eventListItem = dates[i]['events'];
 
-			//Loops every event until eventId in URL matches an event and returns the right event
+			// Loops every event until eventId in URL matches an event and returns the right event
 			for(let j in eventListItem) {
 				const eventLabel = eventListItem[j]['label'];
-				const encodedEventLabel = encodeURIComponent(eventLabel.trim())
+				const encodedEventLabel = encodeURIComponent(eventLabel.trim());
 				if(eventId === encodedEventLabel) {
 					const title = eventLabel;
 					const description = eventListItem[j]['description'];
